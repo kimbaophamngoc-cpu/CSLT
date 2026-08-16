92ec72f0dc94d32d1dd845d7592ef4cb3728a55c
ACER <ACER@LAPTOP-1E21O84L>
2026-08-16 13:56:25 +0700

    Add project files.

---
+++ CSLT/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("XIn chào!");
+        Console.WriteLine("Nice to meet you!");
+    }
+}
